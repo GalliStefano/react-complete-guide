754b7b97e13d3317327a63baf291557154497e6d
Stefano Galli <stefano.galli@publicissapient.com>
2023-05-19 14:22:16 +0200

    update expense filter + form added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -23,9 +24,15 @@ const App = () => {
     }
   ];
   
+  const expenseHandler = expense => {
+    console.log('In app.js')
+    console.log(expense)
+
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={expenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
