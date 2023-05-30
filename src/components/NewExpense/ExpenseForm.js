cde5c9e69316308e44272bec6554b32ec36949d6
Stefano Galli <stefano.galli@publicissapient.com>
2023-05-30 10:15:13 +0200

    chartbar wip

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -63,6 +63,7 @@ const ExpenseForm = (props) => {
 				</div>
 			</div>
 			<div className="new-expense__actions">
+				<button type="reset" onClick={props.onReset} >Cancel</button>
 				<button type="submit">Add Expense</button>
 			</div>
 		</form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
 import ExpenseForm from './ExpenseForm.js'
 import './NewExpense.css';
+import {useState} from "react";
 
 const NewExpense = (props) => {
+    const [activeForm, setActiveForm] = useState(false)
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -9,11 +12,25 @@ const NewExpense = (props) => {
         }
         
         props.onAddExpense(expenseData)
+        setActiveForm(false);
     }
     
+    const toggleForm = () => {
+        setActiveForm(!activeForm);
+    }
+
+    if (activeForm) {
+        return (<div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onReset={toggleForm}/>
+        </div>)
+    }
+
     return (<div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
-    </div>)   
+        <button type="button" onClick={toggleForm}>Add New Expense</button>
+    </div>)
+
+
+
 }
 
 export default NewExpense;
