a390aa841800e65d142857b6d17174ac4488bfcb
Stefano Galli <stefano.galli@publicissapient.com>
2023-05-30 15:49:37 +0200

    update expense chartbar

--- src/components/Chart/chart.js
+++ src/components/Chart/chart.js
@@ -3,9 +3,12 @@ import ChartBar from "./ChartBar";
 
 
 const Chart = props => {
+	const dataPointsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+	const totalMaximum = Math.max(...dataPointsValues);
+
 	return <div className="chart">
 		{props.dataPoints.map(dataPoint => (
-			<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>
+			<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label}/>
 		))}
 	</div>
 }
